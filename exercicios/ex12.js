63a149d32c2d8476a916008638ae89f937ea7e85
Joao <jotadev7@gmail.com>
2021-12-30 14:11:09 -0300

    Ex12 - Condições em JS, e arquivo JS puro

---
+++ exercicios/ex12.js
@@ -0,0 +1,13 @@
+var vel = 90;
+console.log(`A velocidade do seu carro é ${vel}KM/h.`);
+if(vel > 80) { // Condição simples
+    console.log('Você ultrapassou a velocidade máxima permitida.');
+}
+
+var país = 'Brasil';
+console.log(`Vivendo em ${país}`)
+if (país == 'Brasil') { // Condição composta
+    console.log('Você é Brasileiro!');
+} else {
+    console.log('Você é Estrangeiro!');
+}
