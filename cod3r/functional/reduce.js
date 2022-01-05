8ee5c41c849301d48643f5b0974b9b6481fb1d82
Joao <jotadev7@gmail.com>
2022-01-05 09:03:45 -0300

    Estudos JS - Cod3r (Função Reduce)

--- cod3r/functional/reduce.js
+++ cod3r/functional/reduce.js
@@ -1,1 +1,20 @@
-// Diferente do map e do filter, 
+// Diferente do map e do filter, primeiro parâmetro é um acumulador e o segundo parâmetro é o elemento atual. 
+
+const numbers = [1, 2, 3, 4, 5, 6];
+
+const sum = (total, el) => total + el;
+const total = numbers.reduce((total, el) => total + el);
+const total1 = numbers.reduce(sum);
+console.log(total);
+console.log(total1);
+
+const media = (acc, el, i, array) => {
+    if(i === array.length - 1) {
+        return (acc + el) / array.length;
+    } else {
+        return acc + el;
+    }
+}
+
+const result = numbers.reduce(media);
+console.log(`A média de ${numbers[0]}, ${numbers[1]}, ${numbers[2]}, ${numbers[3]}, ${numbers[4]}, ${numbers[5]} é: ${result}.`);
