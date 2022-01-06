b4b4daa2da99ae026410d16d83486a2680661c5d
Joao <jotadev7@gmail.com>
2022-01-06 16:37:31 -0300

    Estudos JS - Cod3r (Função PROMISE)

---
+++ cod3r/curso_rxjs/promise_2.js
@@ -0,0 +1,27 @@
+// Exemplo de callback (hell)
+
+setTimeout(function() {
+    console.log('Executando callback...');
+
+    setTimeout(function() {
+        console.log('Executando callback...');
+
+        setTimeout(function() {
+            console.log('Executando callback...');
+        }, 2000)
+    }, 2000)
+}, 2000)
+
+// Exemplo de promise 
+
+function esperarPor(tempo = 2000) {
+    return new Promise(function(resolve) {
+        setTimeout(function() {
+            console.log('Executando promise...');
+            resolve();
+        }, tempo)
+    })
+}
+
+// esperarPor(3000).then(texto => console.log(texto));
+esperarPor().then(() => esperarPor()).then(esperarPor).then(esperarPor);
