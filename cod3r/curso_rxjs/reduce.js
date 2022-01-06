1b5d8e2e26df7e10df2438a3921ef2fb361623bb
Joao <jotadev7@gmail.com>
2022-01-06 16:09:31 -0300

    Estudos JS - Cod3r (Função REDUCE)

--- cod3r/curso_rxjs/reduce.js
+++ cod3r/curso_rxjs/reduce.js
@@ -17,3 +17,18 @@ console.log(totais);
 
 const totalGeral = totais.reduce(somar);
 console.log(totalGeral);
+
+// Meu próprio Reduce.
+
+Array.prototype.meuReduce = function(fn, inicial) {
+    let acc = inicial;
+
+    for (let i = 0; i < this.length; i++) {
+        if(!acc && i === 0) {
+            acc = this[i];
+            continue;
+        }
+        acc = fn(acc, this[i], i, this);
+    }
+    return acc;
+}
